5fb67817b3997e8df057546ecaf688bd21138cef
Yasas Senarath <12231659+ysenarath@users.noreply.github.com>
2025-12-27 12:39:53 -0500

    Add cheatsheet route to render cheatsheet.html

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/cheatsheet")
+def cheatsheet():
+    return render_template("cheatsheet.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True, port=5066)
